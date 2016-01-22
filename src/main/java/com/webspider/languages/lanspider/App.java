51b33d35dfea0ff6e70e4bdbf109c1c6b44b6d54
rui01.li <rui01.li@vipshop.com>
2016-01-22 18:02:24 +0800

    src/

---
+++ src/main/java/com/webspider/languages/lanspider/App.java
@@ -0,0 +1,13 @@
+package com.webspider.languages.lanspider;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
